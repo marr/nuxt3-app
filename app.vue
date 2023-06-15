d78d87ffc0d7aed810a1006ec4e222e85ab35ca6
David Marr <david.marr@contextlabs.com>
2023-06-15 08:28:02 -0400

    add mapbox

--- app.vue
+++ app.vue
@@ -1,8 +1,45 @@
 <script setup lang="ts">
-const { data } = await useFetch("/api/hello");
+import mapbox from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+const map = ref(null);
+const controls = ref<{
+  bearing: number;
+  center: {
+    lat: number;
+    lng: number
+  };
+  pitch: number;
+  zoom: number;
+}>({
+  bearing: 0,
+  center: {lng: -70.81447095562953, lat: 41.97431913275241},
+  pitch: 47.5,
+  zoom: 7.85
+});
+
+const { mapboxToken } = useRuntimeConfig().public;
+
+onMounted(() => {
+  const mb = new mapbox.Map({
+    container: map.value!,
+    accessToken: mapboxToken,
+    style: 'mapbox://styles/mapbox/streets-v12',
+    ...controls.value
+  });
+  mb.on('idle', () => {
+    controls.value.bearing = mb.getBearing();
+    controls.value.zoom = mb.getZoom();
+    controls.value.center = mb.getCenter();
+    controls.value.pitch = mb.getPitch();
+  })
+})
 </script>
 <template>
   <div>
-    {{ data }}
+    <div ref="map" style="width: 800px; height: 650px"/>
+    <div style="white-space: pre; font-family: system-ui">
+      {{ controls }}
+    </div>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,6 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   runtimeConfig: {
-    apiKey: ''
+    mapboxToken: ''
   }
 })
